c8bfcbcc4f33633ef9b2edbc19b495a8de85e68c
Napsta <dbarnier13@outlook.fr>
2025-05-30 09:50:58 +0200

    système de traduction fini

--- js/translations.js
+++ js/translations.js
@@ -34,6 +34,53 @@ const translations = {
         // Footer
         'footer-text': `Étudiant en deuxième année de BUT Informatique à l'IUT Montpellier-Sète`,
         'footer-rights': '© 2025 Dorian BARNIER. Tous droits réservés.',
+
+        // Page projets*
+        'project-skills': `Compétences demandées`,
+        'project-group': `Travail en groupe`,
+        'project-agile': `Méthodes agiles`,
+        'project-duo': `Travail en binôme`,
+
+        // Project Php
+        'php-title': `Projet Php - Site d'avis de poursuite d'étude`,
+        'php-text1': `Dans un contexte où les parcours académiques deviennent de plus en plus diversifiés et compétitifs, 
+        les établissements d'enseignement supérieur sont confrontés à devoir évaluer efficacement le candidatures des étudiants 
+        en vue de leur admission ou de leur poursuite d'études.`,
+        'php-text2': `Les processus d’évaluation actuels sont souvent peu ergonomiques, manuels, et dispersés entre
+                    différents
+                    outils, ce qui peut engendrer une perte d’efficacité.`,
+        'php-text3': `Ce projet vise à répondre à ce besoin en proposant une plateforme numérique sous forme de site web
+                    dédiée aux universités ainsi qu’au responsable de poursuite d’études du département Informatique de
+                    l’IUT Montpellier-Sète. Celle-ci permet aux jurys d’évaluation d’exprimer leur avis sur la
+                    candidature
+                    de chaque élève en utilisant des mentions telles que très favorable, favorable ou réservé ainsi
+                    qu’un commentaire.`,
+        'php-text4': `L'objectif principal de ce projet était à la fois d'appliquer l'apprentissage des méthodes agiles
+                    dans
+                    un projet, mais aussi d'utiliser l'apprentissage du langage Php et l'utilisation de phpMyAdmin`,
+        'php-report': `Rapport de projet`,
+
+        // Projet Java
+        'java-title': `Projet Java - Jeu de société "Trains"`,
+        'java-text1': `Le but de ce projet est de produire une implémentation en Java du jeu de plateau japonais Trains sur
+                    une durée d'un mois.`,
+        'java-text2': `Le principe du jeu est de construire un réseau ferroviaire en plaçant des rails sur un plateau
+                    réprésentant la région autour de Tokyo ou d'Osaka. Chacun à leur tour les joueurs jouent des cartes
+                    qui
+                    permettent à la fois d'acheter de nouvelles cartes pour améliorer leur deck et de placer des rails
+                    ou des
+                    villes sur le plateau de jeu pour améliorer leur réseau ferroviaire.`,
+        'java-text3': `Ce projet avait pour but de mettre en œuvre les diverses compétences (développement orienté objets,
+                    qualité de développement, algorithmes de graphes et interfaces homme-machine) apprises tout au long
+                    de l'année
+                    dans un projet d'une plus grande envergure que tout nos autres projets de groupes faits
+                    précédemment.`,
+
+        // Projet NDI
+        'ndi-title': `Projet NDI - Site web sur l'eau et l'environnement`,
+        'ndi-text': `Lors d'un évènement nommé "La nuit de l'info" nous avons du réaliser en groupe un site web dans la nuit sur le
+                    thème de l'eau et l'environnement.`,
+        'ndi-website': `Le site web`,
     },
     en: {
         // Navigation
@@ -70,6 +117,53 @@ const translations = {
         // Footer
         'footer-text': `Second year student of Computer Science BUT at the IUT Montpellier-Sète`,
         'footer-rights': '© 2025 Dorian BARNIER. All rights reserved.',
+
+        // Page projets*
+        'project-skills': `Skills required`,
+        'project-group': `Group work`,
+        'project-agile': `Agile methods`,
+        'project-duo': `Work in pairs`,
+
+        // Projet Php
+        'php-title': `Php Project - Continuing Study Notice Website`,
+        'php-text1': `In a context where academic careers are becoming increasingly diverse and competitive, 
+        higher education institutions are faced with the need to effectively assess student applications 
+        for admission or further study.`,
+        'php-text2': `Current assessment processes are often unergonomic, manual and dispersed between
+                    different
+                    tools, which can lead to a loss of efficiency.`,
+        'php-text3': `This project aims to meet this need by offering a digital platform in the form of a website
+                    dedicated to universities and the Head of Continuing Studies in the IT Department of
+                    the IUT Montpellier-Sète. This allows the evaluation juries to express their opinion on the
+                    application
+                    of each student using terms such as very favorable, favorable or reserved as well
+                    just a comment.`,
+        'php-text4': `The main objective of this project was both to apply agile learning methods
+                    in
+                    a project, but also to use learning the Php language and using phpMyAdmin`,
+        'php-report': `Project report`,
+
+        // Projet Java
+        'java-title': `Java Project - Board game "Trains"`,
+        'java-text1': `The goal of this project is to produce a Java implementation of the Japanese board game Trains on
+                    a period of one month.`,
+        'java-text2': `The principle of the game is to build a railway network by placing rails on a platform
+                    representing the area around Tokyo or Osaka. Players play cards in turn
+                    who
+                    Allow both to buy new cards to improve their deck and place rails
+                    or the
+                    cities on the game board to improve their rail network.`,
+        'java-text3': `This project aimed to implement the various skills (object-oriented development,
+                    development quality, graph algorithms and human-machine interfaces) learned throughout
+                    of the year
+                    in a larger project than any of our other group projects made
+                    previously.`,
+
+        // Projet NDI
+        'ndi-title': `NDI Project - Water and environment website`,
+        'ndi-text': `During an event called "The night of the info" we had to realize a website in the night on the
+                    water and the environment.`,
+        'ndi-website': `The website`,
     }
 };
 
